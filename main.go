bb8b81a5d70bc52113a672b263b3cde9e1ae3f40
popsps <59066663+popsps@users.noreply.github.com>
2020-04-12 16:40:52 -0400

    removed frontend 01

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func setupRoutes() {
 	//	_, _ = fmt.Fprintf(w, "simple server v 0.01")
 	//})
 
-	http.Handle("/", http.FileServer(http.Dir("./frontend/build")))
+	http.Handle("/", http.FileServer(http.Dir("./build")))
 
 	//fs := http.FileServer(http.Dir("../frontend/build"))
 	//http.Handle("/", http.StripPrefix("/", fs))
