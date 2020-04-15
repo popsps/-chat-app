62dc143e25bcaa8de14201186ba270977d10aa02
popsps <59066663+popsps@users.noreply.github.com>
2020-04-15 12:52:24 -0400

    host

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,9 +1,12 @@
 const port = process.env.PORT || 8085
 console.log("client port : ", port)
-const socket = new WebSocket(`wss://chat-app0773.herokuapp.com/:${port}/ws`);
+// const socket = new WebSocket(`wss://chat-app0773.herokuapp.com/:${port}/ws`);
 // const socket = new WebSocket(`ws://localhost:${port}/ws`);
+const socket = new WebSocket(`wss://${window.location.host}/ws`);
 //wss https
 //ws http
+console.log("location host: ", window.location.host)
+console.log("location host: ", window.location.port)
 console.log("location host: ", window.location.hostname)
 let connect = callBack => {
   console.log("Connecting...");
